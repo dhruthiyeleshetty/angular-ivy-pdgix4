d6e06360bca94e80064cea5c876a862ec4304299
dhruthiyeleshetty <dhruthi.ym@gmail.com>
2022-02-24 13:56:18 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,26 +7,48 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(private fb : FormBuilder){}
+  constructor(private fb: FormBuilder) {}
   name = 'Angular ' + VERSION.major;
   typedName;
   public visibleName = '';
   // textBox= new FormControl('');
   profileForm = new FormGroup({
-    textBox: new FormControl('')
-  })
+    textBox: new FormControl(''),
+  });
   public names = [
     {
       firstName: 'Dhruthi',
       lastName: 'Yeleshetty',
     },
     {
+      firstName: 'Damini',
+      lastName: 'Lakshya',
+    },
+    {
       firstName: 'Krishna',
       lastName: 'Kolichala',
     },
   ];
-  updateName(){
+  filteredNameList = [];
+
+  enteredInput() {
+    // if(this.profileForm.get('textBox').value.includes){
+    //   // this.profileForm.get('textBox').setValue('Dhruthi');
+    // };
+    let enteredVal = this.profileForm.get('textBox').value;
+    this.names.filter((nm) => {
+      if (nm.firstName.includes('D')) {
+        console.log(nm);
+        this.filteredNameList.push(nm);
+        // console.log(this.filteredNameList);
+      }
+      // console.log(this.filteredNameList);
+    });
+  }
+
+  updateName() {
     // this.textBox.setValue("Dhruthi");
+    // this.profileForm.get('textBox').setValue('Dhruthi');
   }
   // onKeyInput(event) {
   //   // console.log(event.target.value as HTMLInputElement);
